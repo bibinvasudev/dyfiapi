eee3f2912b3390f56346108dece74ffc74bb7108
nikeshu88 <Nikesh.Panoth@ust-global.com>
2019-10-15 18:49:58 +0530

    Added CORS_ORIGIN_ALLOW_ALL

--- connectingkerala/settings.py
+++ connectingkerala/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'core',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
